2f69d56b747ca2c50c53a3e4dd11f8d9dd1bf2ca
Hardie Botha <hardiebotha@gmail.com>
2020-04-03 16:54:31 -0500

    Added running time exercise

---
+++ running_time.py
@@ -0,0 +1,11 @@
+current_time=6+52/60
+distance1=1
+pace1=8+15/60
+distance2=3
+pace2=7+12/60
+distance3=1
+pace3=8+15/60
+running_time_in_minutes=distance1/pace1+distance2/pace2+distance3/pace3
+running_time = running_time_in_minutes / 60
+arrival_time = current_time + running_time
+print (trunc(arrival_time)+":"+(arrival_time-trunc(arrival_time)*60))
